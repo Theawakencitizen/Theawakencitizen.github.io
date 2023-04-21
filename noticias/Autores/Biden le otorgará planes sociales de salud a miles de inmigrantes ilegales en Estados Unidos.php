c5b27c9c9f6d2c0d65ce64f1f8ad30a6841b251f
Theawakencitizen <93624214+Theawakencitizen@users.noreply.github.com>
2023-04-20 21:03:21 -0400

    Add files via upload

---
+++ noticias/Autores/Biden le otorgará planes sociales de salud a miles de inmigrantes ilegales en Estados Unidos.php
@@ -0,0 +1,62 @@
+<!doctype html><html lang="en">
+
+<!-- metatags para buscador hemeroteca -->
+<meta name="title" content="Biden le otorgará planes sociales de salud a miles de inmigrantes ilegales en Estados Unidos">
+<meta name="author" content="Donai De La Zerda">
+<meta name="date" content="14 Abr, 2023">
+<meta name="lola" content="lola de la noticiax">
+<meta name="genre" content="POLÍTICA">
+<!-- metatags para buscador hemeroteca -->
+  <!-- HTML Meta Tags -->
+  <title>Biden le otorgará planes sociales de salud a miles de inmigrantes ilegales en Estados Unidos</title>
+  <meta name="description" content="Por Donai De La Zerda">
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://awakencitizen.com/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Biden le otorgará planes sociales de salud a miles de inmigrantes ilegales en Estados Unidos">
+  <meta property="og:description" content="Por Donai De La Zerda">
+  <meta property="og:image" content="./image-178.webp">
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="twitter:domain" content="awakencitizen.com">
+  <meta property="twitter:url" content="https://awakencitizen.com/">
+  <meta name="twitter:title" content="Biden le otorgará planes sociales de salud a miles de inmigrantes ilegales en Estados Unidos">
+  <meta name="twitter:description" content="Por Donai De La Zerda">
+  <meta name="twitter:image" content="./image-178.webp">
+
+  <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+<meta charset="utf-8">
+
+<meta property="og:type" content="website">
+<meta property="og:locale" content="en_US">
+<link rel="preconnect" href="https://www.googletagmanager.com/" crossorigin="">
+
+<?php require('./plantillanoticiasparte1.php')?>
+</div></div></div></div><div id="element_40" class="w-object ww_element" data-element-type="rectangle" groupid="group_6" fixed="false"><div id="element_40_content" class="content ww_rectangle"></div></div><div id="element_39" class="w-object ww_element" data-element-type="rectangle" groupid="group_6" fixed="false"><div id="element_39_content" class="content ww_rectangle"></div></div><div id="group_6" class="w-object group" data-element-type="group" data-ww_rwd="{&quot;fullWidth&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false}}" fixed="false" groupelements="[&quot;element_95&quot;,&quot;element_40&quot;,&quot;element_39&quot;]" groupid="wSection_webpage_1_default"><div id="group_6_content" class="content"></div></div><div id="element_36" class="w-object ww_element" data-element-type="text" groupid="wSection_webpage_1_default" fixed="false"><div id="element_36_content" class="content text_content"><div class="ww_text_wrapper forceLineHeight"><p style="font-size: 20px; line-height: 24px; text-align: justify;" textrwdproperties="{&quot;rwdMode_1&quot;:{&quot;fontSize&quot;:&quot;20px&quot;,&quot;lineHeight&quot;:&quot;24px&quot;,&quot;textAlign&quot;:&quot;justify&quot;,&quot;letterSpacing&quot;:&quot;&quot;},&quot;rwdMode_2&quot;:{&quot;fontSize&quot;:&quot;20px&quot;,&quot;lineHeight&quot;:&quot;24px&quot;,&quot;textAlign&quot;:&quot;justify&quot;,&quot;letterSpacing&quot;:&quot;&quot;},&quot;rwdMode_4&quot;:{&quot;fontSize&quot;:&quot;16px&quot;,&quot;lineHeight&quot;:&quot;19px&quot;,&quot;textAlign&quot;:&quot;justify&quot;,&quot;letterSpacing&quot;:&quot;&quot;}}"><span style="color:#0d0d0d;">El presidente de los Estados Unidos, Joe Biden, anunció en las últimas horas que miles de inmigrantes ilegales serán aceptados dentro del sistema de salud estadounidense, a través de planes sociales financiados por el gobierno.
+<br><br>
+Todos los inmigrantes ilegales que se encuentran bajo el programa creado por Obama en 2012 llamado “Acción Diferida para los Llegados en la Infancia” (DACA), informalmente nombrados como “Dreamers” (soñadores), serán introducidos en el sistema de seguridad social a pesar de no tener aportes.
+<br><br>
+Estas personas no tienen ciudadanía estadounidense pero no pueden ser deportados por estar en este programa, que busca ayudar “humanitariamente” a los inmigrantes ilegales que llegaron al país siendo menores de edad.
+<br><br>
+
+A partir de ahora, no solo tendrán el beneficio de poder quedarse en los Estados Unidos sin ciudadanía, si no que recibirán subsidios del gobierno federal para enfrentar sus costos relacionados a la salud y los seguros médicos.
+<br><br>
+
+Se estima que los beneficiarios de este plan social son alrededor de 580.000 inmigrantes, que pasarán a tener todos sus gastos médicos cubiertos dentro del Obamacare.
+<br><br>
+
+Biden a su vez tiene la intención de otorgarle la ciudadanía a estas personas, lo cual no ha podido hacer ya que requiere de una ley de amnistía que debe pasar por el Congreso. Es por eso que mientras tanto, el gobierno demócrata intenta otorgarle todos los beneficios posibles que pueda sin pasar por el Legislativo.
+<br><br>
+
+Mientras Estados Unidos atraviesa la peor crisis migratoria de su historia, ahora el Departamento de Salud y Servicios Sociales se hará cargo de la salud de los inmigrantes ilegales, lo cual envía una fuerte señal a las personas en el extranjero que están pensando en viajar ilegalmente al país.
+<br><br>
+
+La oposición republicana está presionando para que una corte federal frene la medida, y obligue a Biden a pasar dicha decisión por el Congreso. La oposición trumpista prometió derribar los beneficios a los ilegales en las cámaras legislativas haciendo valer su fuerza mayoritaria en Diputados.
+<br><br>
+
+A su vez, en el Senado, controlado por los demócratas, habrá una fuerte oposición ya que hay dos senadores oficialistas, Joe Manchin de West Virginia, y Kristen Sinema, de Arizona, que aunque sean demócratas, avisaron que no votarían por la medida si pasa por el Congreso.</span></p></div></div></div><div id="element_13" class="w-object ww_element" data-element-type="img" groupid="wSection_webpage_1_default" fixed="false"><div id="element_13_content" class="content "><div class="ww_image_wrapper"><img id="element_13_image" alt="" title="" data-ken-burns-type="disabled" class="image-uninitialized" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="1px" height="1px" onload="publicationApp.loadPreJsLazyImage(this);" data-ww_rwd="{&quot;data-lazy-load-src&quot;:{&quot;rwdMode_1&quot;:&quot;./image-178.webp&quot;,&quot;rwdMode_2&quot;:&quot;./image-178.webp&quot;,&quot;rwdMode_4&quot;:&quot;./image-178.webp&quot;},&quot;orginalWidth&quot;:{&quot;rwdMode_1&quot;:2048,&quot;rwdMode_2&quot;:2048,&quot;rwdMode_4&quot;:2048},&quot;wasCropped&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false},&quot;width&quot;:{&quot;rwdMode_1&quot;:786,&quot;rwdMode_2&quot;:786,&quot;rwdMode_4&quot;:323},&quot;backgroundSize&quot;:{&quot;rwdMode_1&quot;:&quot;imageCrop&quot;,&quot;rwdMode_2&quot;:&quot;imageCrop&quot;,&quot;rwdMode_4&quot;:&quot;imageCrop&quot;},&quot;data-isEnabledImageParallax&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false},&quot;backgroundPosition&quot;:{&quot;rwdMode_1&quot;:&quot;50% 50%&quot;,&quot;rwdMode_2&quot;:&quot;50% 50%&quot;,&quot;rwdMode_4&quot;:&quot;50% 50%&quot;},&quot;data-imageParallax&quot;:{&quot;rwdMode_1&quot;:80,&quot;rwdMode_2&quot;:80,&quot;rwdMode_4&quot;:80},&quot;data-used-webp-image&quot;:{&quot;rwdMode_1&quot;:true,&quot;rwdMode_2&quot;:true,&quot;rwdMode_4&quot;:true},&quot;orginalHeight&quot;:{&quot;rwdMode_1&quot;:1152,&quot;rwdMode_2&quot;:1152,&quot;rwdMode_4&quot;:1152},&quot;height&quot;:{&quot;rwdMode_1&quot;:442,&quot;rwdMode_2&quot;:442,&quot;rwdMode_4&quot;:182}}"></div></div></div><div id="element_11" class="w-object ww_element" data-element-type="simpleText" groupid="group_8" fixed="false"><div id="element_11_content" class="content "><h2 class="news_element h2_default_block">14 Abr, 2023&#xa0;</h2></div></div><div id="element_10" class="w-object ww_element" data-element-type="simpleText" groupid="group_8" fixed="false"><div id="element_10_content" class="content "><h2 class="news_element h2_default_block">Por <a href="https://awakencitizen.com/autores/Donai De La Zerda" style="color:#b33828;">Donai De La Zerda</a></h2></div></div><div id="group_8" class="w-object group" data-element-type="group" data-ww_rwd="{&quot;fullWidth&quot;:{&quot;rwdMode_1&quot;:false,&quot;rwdMode_2&quot;:false,&quot;rwdMode_4&quot;:false}}" fixed="false" groupelements="[&quot;element_11&quot;,&quot;element_10&quot;]" groupid="wSection_webpage_1_default"><div id="group_8_content" class="content"></div></div><div id="element_7" class="w-object ww_element" data-element-type="simpleText" groupid="wSection_webpage_1_default" fixed="false"><div id="element_7_content" class="content "><h2 class="news_element h2_default_block">Biden le otorgará planes sociales de salud a miles de inmigrantes ilegales en Estados Unidos</h2></div></div><div id="element_5" class="w-object ww_element" data-element-type="simpleText" groupid="wSection_webpage_1_default" fixed="false"><div id="element_5_content" class="content "><h2 class="news_element h2_default_block">POLÍTICA</h2></div></div>
+<?php require('./plantillanoticiasparte2.php')?>
